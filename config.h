2f03f5729482b6b6de39b6c82cccbfa0921602e4
Wawan <hermawantan12@gmail.com>
2024-05-23 11:51:24 +0800

    nord theme

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 3;        /* gaps between windows */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -12,24 +12,25 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12", "JoyPixels:pixelsize=11:antialias=true:autohint=true"};
+static const char *fonts[]          = { "FiraMono Nerd Font:size=12", "JoyPixels:pixelsize=11:antialias=true:autohint=true"};
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#581845";
-static char selbgcolor[]            = "#581845";
+static char normbordercolor[]       = "#3B4252";
+static char normbgcolor[]           = "#2E3440";
+static char normfgcolor[]           = "#D8DEE9";
+static char selbordercolor[]        = "#434C5E";
+static char selbgcolor[]            = "#434C5E";
+static char selfgcolor[]            = "#ECEFF4";
 
 #include "themes/catppuccin.h"
 static char *colors[][3] = {
        /*               fg           bg           border   */
-         [SchemeNorm]       = { "#fff",   black,  gray2 },
-    	[SchemeSel]        = { gray4,   blue,   blue  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel] =  { selfgcolor,  selbgcolor,  selbordercolor },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
 
 static const Rule rules[] = {
 	/* xprop(1):
